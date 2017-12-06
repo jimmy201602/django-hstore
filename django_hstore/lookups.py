629c6bd03556db4e22be91ac9e030b12f344f820
Jimmy <zhengge2012@gmail.com>
2017-12-06 17:28:00 +0800

    add mysql json query lookup from django-mysql

--- django_hstore/lookups.py
+++ django_hstore/lookups.py
@@ -8,11 +8,14 @@ from django.db.models.lookups import (
     LessThanOrEqual,
     Contains,
     IContains,
-    IsNull
+    IsNull,
+    Exact
 )
+from django.db.models import  Lookup
 
 from django_hstore.utils import get_cast_for_param, get_value_annotations
-
+import json
+import collections
 
 __all__ = [
     'HStoreComparisonLookupMixin',
@@ -138,3 +141,126 @@ class HStoreIsNull(IsNull):
             return (" AND ".join(conditions), lhs_params)
 
         return super(HStoreIsNull, self).as_sql(compiler, connection)
+
+
+
+# JSONField
+class JSONValue(Func):
+    function = 'CAST'
+    template = '%(function)s(%(expressions)s AS JSON)'
+
+    def __init__(self, expression):
+        json_string = json.dumps(expression, allow_nan=False)
+        super(JSONValue, self).__init__(Value(json_string))
+        
+class JSONLookupMixin(object):
+
+    def get_prep_lookup(self):
+        value = self.rhs
+        if not hasattr(value, '_prepare') and value is not None:
+            return JSONValue(value)
+        return super(JSONLookupMixin, self).get_prep_lookup()
+
+
+class JSONExact(JSONLookupMixin, Exact):
+    pass
+
+
+class JSONGreaterThan(JSONLookupMixin, GreaterThan):
+    lookup_name = 'gt'
+
+
+class JSONGreaterThanOrEqual(JSONLookupMixin, GreaterThanOrEqual):
+    lookup_name = 'gte'
+
+
+class JSONLessThan(JSONLookupMixin, LessThan):
+    lookup_name = 'lt'
+
+
+class JSONLessThanOrEqual(JSONLookupMixin, LessThanOrEqual):
+    lookup_name = 'lte'
+
+
+class JSONContainedBy(Lookup):
+    lookup_name = 'contained_by'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = rhs_params + lhs_params
+        return 'JSON_CONTAINS({}, {})'.format(rhs, lhs), params
+
+
+class JSONContains(JSONLookupMixin, Lookup):
+    lookup_name = 'contains'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return 'JSON_CONTAINS({}, {})'.format(lhs, rhs), params
+
+
+class JSONHasKey(Lookup):
+    lookup_name = 'has_key'
+
+    def get_prep_lookup(self):
+        if not isinstance(self.rhs, six.text_type):
+            raise ValueError(
+                "JSONField's 'has_key' lookup only works with {} values"
+                .format(six.text_type.__name__)
+            )
+        return super(JSONHasKey, self).get_prep_lookup()
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        key_name = self.rhs
+        path = '$.{}'.format(json.dumps(key_name))
+        params = lhs_params + [path]
+        return "JSON_CONTAINS_PATH({}, 'one', %s)".format(lhs), params
+
+
+class JSONSequencesMixin(object):
+    def get_prep_lookup(self):
+        if not isinstance(self.rhs, collections.Sequence):
+            raise ValueError(
+                "JSONField's '{}' lookup only works with Sequences"
+                .format(self.lookup_name)
+            )
+        return self.rhs
+
+
+class JSONHasKeys(JSONSequencesMixin, Lookup):
+    lookup_name = 'has_keys'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        paths = [
+            '$.{}'.format(json.dumps(key_name))
+            for key_name in self.rhs
+        ]
+        params = lhs_params + paths
+
+        sql = ['JSON_CONTAINS_PATH(', lhs, ", 'all', "]
+        sql.append(', '.join('%s' for _ in paths))
+        sql.append(')')
+        return ''.join(sql), params
+
+
+class JSONHasAnyKeys(JSONSequencesMixin, Lookup):
+    lookup_name = 'has_any_keys'
+
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        paths = [
+            '$.{}'.format(json.dumps(key_name))
+            for key_name in self.rhs
+        ]
+        params = lhs_params + paths
+
+        sql = ['JSON_CONTAINS_PATH(', lhs, ", 'one', "]
+        sql.append(', '.join('%s' for _ in paths))
+        sql.append(')')
+        return ''.join(sql), params
+
